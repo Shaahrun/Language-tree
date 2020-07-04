5582b900886bc0376803b7a41cbc7cf952436ffd
Shaahrun <54306347+Shaahrun@users.noreply.github.com>
2020-07-04 04:31:25 +0100

    Create SinoTibetan.java

---
+++ SinoTibetan.java
@@ -0,0 +1,9 @@
+class SinoTibetan extends Language {
+  
+  public SinoTibetan(String langName, int speakers, "Asia", "subject-object-verb") {
+      if (langName.contains("Chinese")) {
+        this.wordOrder = "subject-verb-object";
+      }
+  }
+  
+}
